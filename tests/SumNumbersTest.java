58fbd4fbb3b8b699959a3fd1ed0ab659a78774f7
Jesus C <mannychacon2005@gmail.com>
2025-02-25 23:48:27 -0800

    Ready to grade!

--- tests/SumNumbersTest.java
+++ tests/SumNumbersTest.java
@@ -1,2 +1,16 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SumNumbersTest {
+
+    @org.junit.jupiter.api.Test
+    void sumIsEven() {
+        SumNumbers test = new SumNumbers();
+        assertEquals(1,test.sumIsEven(1,1));
+    }
+
+    @org.junit.jupiter.api.Test
+    void sumIsOdd() {
+        SumNumbers test = new SumNumbers();
+        assertEquals(0,test.sumIsEven(1,2));
+    }
 }
--- tests/TipCalculatorTest.java
+++ tests/TipCalculatorTest.java
@@ -1,2 +1,15 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TipCalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void TipCalculatorTest1() {
+        TipCalculator test = new TipCalculator();
+        assertEquals(20,test.calculateTip(100,20));
+    }
+    @org.junit.jupiter.api.Test
+    void TipCalculatorTest2() {
+        TipCalculator test = new TipCalculator();
+        assertEquals(500,test.calculateTip(1000,50));
+    }
 }
